414f37065751050b86a240a85e7cf72e3bd433d8
Edrich Rabanes <rabanesedrich@gmail.com>
2025-10-14 00:32:10 -0700

    added comments and header

--- 3.8#5.py
+++ 3.8#5.py
@@ -1,21 +1,27 @@
+# ECEGR-2000
+# Exercise 5 
+# Author: Edrich Rabanes
+
+# predefined testing lists
 fruits = ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana']
 vegetables = ['eggplant', 'broccoli', 'carrot', 'cauliflower', 'zucchini']
 
 def combine_and_sort_two_lists(list1, list2):
     newList = []
     
-    if (isinstance(list1, list) and isinstance(list2, list)):
-        for i in range(len(list1)):
-            newList.append(list1[i])
+    if (isinstance(list1, list) and isinstance(list2, list)): # checks if both inputs are lists
+        for i in range(len(list1)): 
+            newList.append(list1[i]) # adds everything in first list to new list
      
         for k in range(len(list2)):
-            newList.append(list2[k])
+            newList.append(list2[k]) # adds everything to second list to new list
         
-        return sorted(newList)
+        return sorted(newList) # sorts out the combined list
     else:
-        print("Error in combine_and_sort_two_lists: one argument is not a list")
+        print("Error in combine_and_sort_two_lists: one argument is not a list") # error msg if an argument is not a list
         return "None"
 
+# print tests to ensure code runs correctly
 print(combine_and_sort_two_lists(fruits, vegetables))
 print(fruits)
 print(vegetables)
